24f901f6500cc41efc0cd312d766ec6996022784
MirkoCifelli <ciffo4@hotmail.it>
2024-02-23 15:09:39 +0100

    add  header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,40 +2,77 @@
     $links = [
         [
             'url' => '/',
-            'label' => 'Home',
-            'active' => true,
-        ],
-        [
-            'url' => '/chi-siamo',
-            'label' => 'Chi siamo',
-            'active' => true,
-        ],
-        [
-            'url' => '/contatti',
-            'label' => 'Contatti',
+            'label' => 'Characters',
             'active' => false,
         ],
-    ];
+        [
+            'url' => '/comics',
+            'label' => 'Comics',
+            'active' => true,
+        ],
+        [
+            'url' => '/movie',
+            'label' => 'Movie',
+            'active' => false,
+        ],
+        [
+            'url' => '/tv',
+            'label' => 'TV',
+            'active' => false,
+        ],
+        [
+            'url' => '/games',
+            'label' => 'Games',
+            'active' => false,
+        ],
+        [
+            'url' => '/collectibles',
+            'label' => 'Collectibles',
+            'active' => false,
+        ],
+        [
+            'url' => '/videos',
+            'label' => 'Videos',
+            'active' => false,
+        ],
+        [
+            'url' => '/fans',
+            'label' => 'Fans',
+            'active' => false,
+        ],
+        [
+            'url' => '/news',
+            'label' => 'News',
+            'active' => false,
+        ],
+        [
+            'url' => '/shop',
+            'label' => 'Shop',
+            'active' => false,
+        ]
+ ];
 @endphp
 
 <header>
     <div class="container">
         <div class="row">
-            <div class="col">
-
+            <div class="col-2">
+                <div class="img-logo p-3">
+                    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+                </div>
             </div>
-            <div class="col">
+            <div class="col-8">
                 
                     <nav>
-                        <ul class="d-flex justify-content-between ">
+                        <ul class="d-flex justify-content-between list-unstyled p-4">
                             @foreach ($links as $link)
-                                <li>
+                                <li class="m-2">
                                     @if ($link['active'])
-                                        <a href="{{ $link['url'] }}">
+                                        <a href="{{ $link['url'] }}" class=" text-uppercase ">
                                             {{ $link['label'] }}
                                         </a>
                                     @else
-                                        <del>
+                                        <del class="text-uppercase">
                                             {{ $link['label'] }}
                                         </del>
                                     @endif
@@ -45,7 +82,11 @@
                     </nav>
                 
             </div>
-            <div class="col"></div>
+            <div class="col-2">
+                <div class="mt-4">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                </div>
+            </div>
         </div>
     </div>
 </header>
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     $comics = config('comics');
 
-    dd($comics);
+    // dd($comics);
 
 
     $firstName = 'Gino';
@@ -27,11 +27,11 @@ Route::get('/', function () {
         'firstName' => $firstName,
         'lastName' => $lastName,
     ]);
-    // return view('welcome', compact('firstName', 'lastName'));
+    
 });
 
 Route::get('/chi-siamo', function () {
     return view('subpages.about');
 });
 
-// Route::get(PERCORSO CON CUI ARRIVARE ALLA PAGINA, FUNZIONE DI CALLBACK CHE MI CREA LA RISPOSTA DA DARE ALL UTENTE)
+
